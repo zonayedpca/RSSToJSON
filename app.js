2e07bdeaf52b49f8dc1707d6dc0b4dd0bf8cebb7
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-03-13 18:08:08 +0530

    comments added

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const getPosts = require('./get/getPosts'),
 const URL = 'https://medium.com/feed/@zonayedpca';
 
 getPosts(URL).then((res) => {
+  // res has the json document, you can fo whatever you want
+
+  // Just a demonstration what can be done
   const file = JSON.stringify(res, undefined, 2);
   fs.writeFile('blog.txt', file, (err) => {
     if (err) throw err;
